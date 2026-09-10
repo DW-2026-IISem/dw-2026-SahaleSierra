d63e48a3452a376f7029e7f1353db9fbac09da5c
SahaleSierra <sahalesierra75@gmail.com>
2026-09-10 01:33:09 -0500

    8.23 actualizamos business.module con ProductTypesModule

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module.js';
+import { ProductTypesModule } from './product-types/product-types.module.js';
 
 @Module({
-  imports: [ClientsModule],
-  exports: [ClientsModule],
+  imports: [ClientsModule, ProductTypesModule],
+  exports: [ClientsModule, ProductTypesModule],
 })
 export class BusinessModule {}
