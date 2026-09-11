91388422a82d989494ec0d34e2013915292c19cf
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 18:12:50 -0500

    7.26 agregamos patients module a business module

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PatientsModule } from './patients/patients.module.js';
 
 @Module({
-  imports: [],
-  exports: [],
+  imports: [PatientsModule],
+  exports: [PatientsModule],
 })
 export class BusinessModule {}
