1adee6f44269ad6295191e3ee8b00498cca76ae8
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 18:05:25 -0500

    7.17 agregamos use case eliminar paciente

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/patients/application/use-cases/delete-patient.use-case.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PatientNotFoundException } from '../../domain/exceptions/patient-not-found.exception.js';
+import {
+  PATIENT_REPOSITORY,
+  PatientRepository,
+} from '../../domain/interfaces/patient-repository.interface.js';
+
+@Injectable()
+export class DeletePatientUseCase {
+  constructor(
+    @Inject(PATIENT_REPOSITORY) private readonly patientRepository: PatientRepository,
+  ) {}
+
+  async execute(id: number): Promise<void> {
+    const patient = await this.patientRepository.findById(id);
+    if (!patient) {
+      throw new PatientNotFoundException(id);
+    }
+    await this.patientRepository.delete(id);
+  }
+}
