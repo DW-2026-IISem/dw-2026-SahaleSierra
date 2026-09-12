6925f7dda2ac57e802792836d3604981a4528a91
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 19:09:08 -0500

    8.13 agregamos use case obtener especialidad

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/specialties/application/use-cases/get-specialty.use-case.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Specialty } from '../../domain/entities/specialty.entity.js';
+import { SpecialtyNotFoundException } from '../../domain/exceptions/specialty-not-found.exception.js';
+import {
+  SPECIALTY_REPOSITORY,
+  SpecialtyRepository,
+} from '../../domain/interfaces/specialty-repository.interface.js';
+
+@Injectable()
+export class GetSpecialtyUseCase {
+  constructor(
+    @Inject(SPECIALTY_REPOSITORY) private readonly specialtyRepository: SpecialtyRepository,
+  ) {}
+
+  async execute(id: number): Promise<Specialty> {
+    const specialty = await this.specialtyRepository.findById(id);
+    if (!specialty) {
+      throw new SpecialtyNotFoundException(id);
+    }
+    return specialty;
+  }
+}
