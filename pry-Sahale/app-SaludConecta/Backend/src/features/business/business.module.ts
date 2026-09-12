1beda01e4b328962a80190b71aeafcf7ca093459
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 22:17:50 -0500

    9.21 registramos doctor model en sequelize factory

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PatientsModule } from './patients/patients.module.js';
 import { SpecialtiesModule } from './specialties/specialties.module.js';
+import { DoctorsModule } from './doctors/doctors.module.js';
 
 @Module({
-  imports: [PatientsModule, SpecialtiesModule],
-  exports: [PatientsModule, SpecialtiesModule],
+  imports: [PatientsModule, SpecialtiesModule, DoctorsModule],
+  exports: [PatientsModule, SpecialtiesModule, DoctorsModule],
 })
 export class BusinessModule {}
