0c84d888751e2cfa006d47fe79c8a691ac8fda1a
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 19:16:29 -0500

    8.21 conectamos seeder de especialidades al seeder general

--- pry-Sahale/app-SaludConecta/Backend/src/infrastructure/database/sequelize/database-seeder.service.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/infrastructure/database/sequelize/database-seeder.service.ts
@@ -1,2 +1,4 @@
 // import { seedPatients } from '../../../features/business/patients/infrastructure/persistence/seeders/patients.seeder.js';
 // await seedPatients();
+// import { seedSpecialties } from '../../../features/business/specialties/infrastructure/persistence/seeders/specialties.seeder.js';
+// await seedSpecialties();
