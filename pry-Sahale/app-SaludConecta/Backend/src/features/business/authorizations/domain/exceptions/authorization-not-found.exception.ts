79817145bc8ec6a15bbc4d6849608fa77716d673
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 14:02:33 -0500

    14.2 agregamos excepcion autorizacion no encontrada

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/authorizations/domain/exceptions/authorization-not-found.exception.ts
@@ -0,0 +1,7 @@
+import { EntityNotFoundException } from '../../../../../common/exceptions/entity-not-found.exception.js';
+
+export class AuthorizationNotFoundException extends EntityNotFoundException {
+  constructor(id: number | string) {
+    super('Autorización', id);
+  }
+}
