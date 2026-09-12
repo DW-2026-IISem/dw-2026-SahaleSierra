15b47799e7f89975d82d56f0474c8f23ec943060
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 17:28:57 -0500

    17.21 agregamos use case obtener factura

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/invoices/application/use-cases/get-invoice.use-case.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Invoice } from '../../domain/entities/invoice.entity.js';
+import { InvoiceNotFoundException } from '../../domain/exceptions/invoice-not-found.exception.js';
+import { INVOICE_REPOSITORY } from '../../domain/interfaces/invoice-repository.interface.js';
+import type { InvoiceRepository } from '../../domain/interfaces/invoice-repository.interface.js';
+
+@Injectable()
+export class GetInvoiceUseCase {
+  constructor(@Inject(INVOICE_REPOSITORY) private readonly invoiceRepository: InvoiceRepository) {}
+
+  async execute(id: number): Promise<Invoice> {
+    const invoice = await this.invoiceRepository.findById(id);
+    if (!invoice) throw new InvoiceNotFoundException(id);
+    return invoice;
+  }
+}
