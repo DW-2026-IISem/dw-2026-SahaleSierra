0173d2b2c939544e72597453537b412d1dc80af3
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 16:44:09 -0500

    16.23 agregamos barrel de encounters

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/encounters/index.ts
@@ -0,0 +1,4 @@
+export * from './domain/entities/encounter.entity.js';
+export * from './domain/enums/encounter-status.enum.js';
+export * from './domain/interfaces/encounter-repository.interface.js';
+export * from './encounters.module.js';
