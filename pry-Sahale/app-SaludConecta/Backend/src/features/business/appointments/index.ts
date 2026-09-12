00c1aabc21af3cf8aa53f9943683bbaefa831ff9
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 13:42:08 -0500

    13.23 agregamos barrel de appointments

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/appointments/index.ts
@@ -0,0 +1,4 @@
+export * from './domain/entities/appointment.entity.js';
+export * from './domain/enums/appointment-status.enum.js';
+export * from './domain/interfaces/appointment-repository.interface.js';
+export * from './appointments.module.js';
