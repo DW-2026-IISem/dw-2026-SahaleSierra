cd5ab2f636e41e280f2ba6ff57334a86eb45b02b
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 11:42:20 -0500

    11.24 agregamos services module a business module

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -3,9 +3,10 @@ import { PatientsModule } from './patients/patients.module.js';
 import { SpecialtiesModule } from './specialties/specialties.module.js';
 import { DoctorsModule } from './doctors/doctors.module.js';
 import { DoctorSpecialtiesModule } from './doctor-specialties/doctor-specialties.module.js';
+import { ServicesModule } from './services/services.module.js';
 
 @Module({
-  imports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule],
-  exports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule],
+  imports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule, ServicesModule],
+  exports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule, ServicesModule],
 })
 export class BusinessModule {}
