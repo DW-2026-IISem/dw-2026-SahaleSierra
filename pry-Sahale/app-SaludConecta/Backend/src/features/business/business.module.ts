0ed3b4ba1cad3b2dbb61265148d30f18e66c5ca8
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 10:47:32 -0500

    10.21 agregamos doctor specialties module a business module

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PatientsModule } from './patients/patients.module.js';
 import { SpecialtiesModule } from './specialties/specialties.module.js';
 import { DoctorsModule } from './doctors/doctors.module.js';
+import { DoctorSpecialtiesModule } from './doctor-specialties/doctor-specialties.module.js';
 
 @Module({
-  imports: [PatientsModule, SpecialtiesModule, DoctorsModule],
-  exports: [PatientsModule, SpecialtiesModule, DoctorsModule],
+  imports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule],
+  exports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule],
 })
 export class BusinessModule {}
