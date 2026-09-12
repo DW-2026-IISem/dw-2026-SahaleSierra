4d68a1c10077aedc0db9baf8cecb95b48f84ca5d
SahaleSierra <sahalesierra75@gmail.com>
2026-09-11 22:09:21 -0500

    9.15 agregamos use case obtener medico

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/doctors/application/use-cases/get-doctor.use-case.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Doctor } from '../../domain/entities/doctor.entity.js';
+import { DoctorNotFoundException } from '../../domain/exceptions/doctor-not-found.exception.js';
+import {
+  DOCTOR_REPOSITORY,
+  DoctorRepository,
+} from '../../domain/interfaces/doctor-repository.interface.js';
+
+@Injectable()
+export class GetDoctorUseCase {
+  constructor(
+    @Inject(DOCTOR_REPOSITORY) private readonly doctorRepository: DoctorRepository,
+  ) {}
+
+  async execute(id: number): Promise<Doctor> {
+    const doctor = await this.doctorRepository.findById(id);
+    if (!doctor) {
+      throw new DoctorNotFoundException(id);
+    }
+    return doctor;
+  }
+}
