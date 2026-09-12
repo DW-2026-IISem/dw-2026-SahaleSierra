a31824ff09377c542cc2970eb112256b0da13f49
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 10:45:12 -0500

    10.18 agregamos barrel de doctor specialties

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/doctor-specialties/index.ts
@@ -0,0 +1,3 @@
+export * from './domain/entities/doctor-specialty.entity.js';
+export * from './domain/interfaces/doctor-specialty-repository.interface.js';
+export * from './doctor-specialties.module.js';
