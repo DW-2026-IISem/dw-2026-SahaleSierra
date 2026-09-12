afe53d1573ca44f6e6738d443cff05209f580cd2
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 14:49:48 -0500

    15.22 agregamos barrel de clinical records

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/clinical-records/index.ts
@@ -0,0 +1,3 @@
+export * from './domain/entities/clinical-record.entity.js';
+export * from './domain/interfaces/clinical-record-repository.interface.js';
+export * from './clinical-records.module.js';
