36dd4c57b14605e6acecfcc5909318a01e63b8a1
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 13:34:33 -0500

    13.17 agregamos use case cancelar cita

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/appointments/application/use-cases/cancel-appointment.use-case.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Appointment } from '../../domain/entities/appointment.entity.js';
+import { AppointmentNotFoundException } from '../../domain/exceptions/appointment-not-found.exception.js';
+import { APPOINTMENT_REPOSITORY } from '../../domain/interfaces/appointment-repository.interface.js';
+import type { AppointmentRepository } from '../../domain/interfaces/appointment-repository.interface.js';
+
+@Injectable()
+export class CancelAppointmentUseCase {
+  constructor(
+    @Inject(APPOINTMENT_REPOSITORY) private readonly appointmentRepository: AppointmentRepository,
+  ) {}
+
+  async execute(id: number): Promise<Appointment> {
+    const appointment = await this.appointmentRepository.findById(id);
+    if (!appointment) throw new AppointmentNotFoundException(id);
+
+    appointment.cancel();
+    return this.appointmentRepository.update(id, appointment);
+  }
+}
