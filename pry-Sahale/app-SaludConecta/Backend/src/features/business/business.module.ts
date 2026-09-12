cad76fb6b7d695d5be43ab46cc0c151235bfb7f4
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 12:19:45 -0500

    12.22 registramos agenda model en sequelize factory

--- pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/business.module.ts
@@ -4,9 +4,24 @@ import { SpecialtiesModule } from './specialties/specialties.module.js';
 import { DoctorsModule } from './doctors/doctors.module.js';
 import { DoctorSpecialtiesModule } from './doctor-specialties/doctor-specialties.module.js';
 import { ServicesModule } from './services/services.module.js';
+import { AgendasModule } from './agendas/agendas.module.js';
 
 @Module({
-  imports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule, ServicesModule],
-  exports: [PatientsModule, SpecialtiesModule, DoctorsModule, DoctorSpecialtiesModule, ServicesModule],
+  imports: [
+    PatientsModule,
+    SpecialtiesModule,
+    DoctorsModule,
+    DoctorSpecialtiesModule,
+    ServicesModule,
+    AgendasModule,
+  ],
+  exports: [
+    PatientsModule,
+    SpecialtiesModule,
+    DoctorsModule,
+    DoctorSpecialtiesModule,
+    ServicesModule,
+    AgendasModule,
+  ],
 })
 export class BusinessModule {}
