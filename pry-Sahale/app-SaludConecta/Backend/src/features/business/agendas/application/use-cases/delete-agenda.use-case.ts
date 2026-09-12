689397d292c897b06470dbe11868bf765ad86404
SahaleSierra <sahalesierra75@gmail.com>
2026-09-12 12:15:29 -0500

    12.14 agregamos use case eliminar agenda

---
+++ pry-Sahale/app-SaludConecta/Backend/src/features/business/agendas/application/use-cases/delete-agenda.use-case.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { AgendaNotFoundException } from '../../domain/exceptions/agenda-not-found.exception.js';
+import { AGENDA_REPOSITORY } from '../../domain/interfaces/agenda-repository.interface.js';
+import type { AgendaRepository } from '../../domain/interfaces/agenda-repository.interface.js';
+
+@Injectable()
+export class DeleteAgendaUseCase {
+  constructor(@Inject(AGENDA_REPOSITORY) private readonly agendaRepository: AgendaRepository) {}
+
+  async execute(id: number): Promise<void> {
+    const agenda = await this.agendaRepository.findById(id);
+    if (!agenda) {
+      throw new AgendaNotFoundException(id);
+    }
+    await this.agendaRepository.delete(id);
+  }
+}
